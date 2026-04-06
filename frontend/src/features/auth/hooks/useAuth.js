3834e14bbb03f38cb18ddd727f196456109eead1
Hariom-Jangir <hariomjangir2006@gmail.com>
2026-04-06 22:41:13 +0530

    added loading

--- frontend/src/features/auth/hooks/useAuth.js
+++ frontend/src/features/auth/hooks/useAuth.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../auth.context.jsx";
-import {login ,register, logout, getMe} from "../services/auth.api.js"
+import {login ,register, logout} from "../services/auth.api.js"
 
 
 export const useAuth = () => {
@@ -9,26 +9,46 @@ export const useAuth = () => {
     
     const handleLogin = async ({email, password}) => {
         setLoading(true);
+        try{
         const data = await login({email,password});
         setUser(data.user);
+        }
+        catch(err){
+            console.log(err)
+        }
+        finally{        
         setLoading(false);
+        }
     }
 
     const handleRegister = async ({username,email,password}) => {
         setLoading(true);
+        try{
         const data = await register({username,email,password});
         setUser(data.user);
+        }
+        catch(err){
+            console.log(err)
+        }
+        finally{    
         setLoading(false);
+        }
     }
 
     const handleLogout = async () => {
         setLoading(true);
+        try{
         await logout();
-        setUser(null);
+        }
+        catch(err){
+            console.log(err);
+             setUser(null);
+        }
+        finally{    
+       
         setLoading(false);
     }
-
-    
+}
 
     return {user,loading,handleLogin,handleRegister,handleLogout};
 }
